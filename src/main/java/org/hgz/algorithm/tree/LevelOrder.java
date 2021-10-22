c90c87530c4c9e3a807694fd40439d17758d1f8f
guozhong.huang <hgz199102@163.com>
2021-10-22 09:50:03 +0800

    二叉树的广度优先遍历

--- src/main/java/org/hgz/algorithm/tree/LevelOrder.java
+++ src/main/java/org/hgz/algorithm/tree/LevelOrder.java
@@ -1,32 +1,62 @@
 package org.hgz.algorithm.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelOrder {
     public List<List<Integer>> levelOrder(Node root) {
-
         List<List<Integer>> result = new ArrayList<>();
-        recur(root, 0, result);
+        dfs(root, 0, result);
         return result;
 
     }
 
-    public void recur(Node root, int leve, List<List<Integer>> result) {
-        if (root != null) {
-
-            if(result.size() <= leve) {
-                result.add(new ArrayList<>());
-            }
-            result.get(leve).add(root.val);
-
-            for(Node chilren : root.children) {
-                recur(chilren, leve + 1, result);
-            }
-
+    public void dfs(Node root, int level, List<List<Integer>> result) {
+        if (root == null) {
+            return;
         }
 
+        if (result.size() <= level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        List<Node> children = root.children;
+        for (int i = 0; i < children.size(); i++) {
+            dfs(children.get(i), level + 1, result);
+        }
     }
 
+    /**
+     * 广度优先搜索
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> bfs(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if(root == null) {
+            return ret;
+        }
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            List<Integer> tempRet = new ArrayList<>();
+            ret.add(tempRet);
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                tempRet.add(poll.val);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+        }
+        return ret;
+    }
 }
